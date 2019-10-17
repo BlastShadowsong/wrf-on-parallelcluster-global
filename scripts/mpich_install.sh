0c22d9b7ab867427ab07d24d5f389aaa7bc87f70
BlastShadowsong <lisiyuanwelcome@163.com>
2019-10-17 18:03:06 +0800

    Update mpich version

--- scripts/mpich_install.sh
+++ scripts/mpich_install.sh
@@ -4,9 +4,9 @@
 #install
 source ~/.bashrc
 cd /fsx
-wget http://www2.mmm.ucar.edu/wrf/OnLineTutorial/compile_tutorial/tar_files/mpich-3.0.4.tar.gz
-tar xzvf mpich-3.0.4.tar.gz
-cd mpich-3.0.4
+wget http://www.mpich.org/static/downloads/3.3.1/mpich-3.3.1.tar.gz
+tar xzvf mpich-3.3.1.tar.gz
+cd mpich-3.3.1
 ./configure --prefix=/fsx/mpich
 make
 make install
